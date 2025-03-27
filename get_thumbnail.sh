50dce4ff541814308bada56d15fe2001b31429eb
ludercej <p4nda.soft@gmail.com>
2025-03-27 22:48:25 +0100

    modif .yaml v32

--- get_thumbnail.sh
+++ get_thumbnail.sh
@@ -6,6 +6,9 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+# Créer le répertoire 'uploads' si ce n'est pas déjà fait
+mkdir -p uploads
+
 # Extraire l'ID de la vidéo à partir de l'URL
 URL=$1
 VIDEO_ID=$(echo $URL | sed -E 's/.*(v=|\/)([a-zA-Z0-9_-]+).*/\2/')
@@ -15,9 +18,6 @@ if [ -z "$VIDEO_ID" ]; then
   echo "Erreur : Impossible d'extraire l'ID de la vidéo."
   exit 1
 fi
-
-# Créer le répertoire 'uploads' si ce n'est pas déjà fait
-mkdir -p uploads
 
 # Télécharger la miniature en haute définition
 THUMBNAIL_URL="https://img.youtube.com/vi/$VIDEO_ID/maxresdefault.jpg"
