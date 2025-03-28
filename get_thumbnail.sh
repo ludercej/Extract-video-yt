65f30685941466b5f5adb5ba592d7ac2bce8ff1b
ludercej <p4nda.soft@gmail.com>
2025-03-28 10:04:40 +0100

    modif .yaml v49

--- get_thumbnail.sh
+++ get_thumbnail.sh
@@ -2,25 +2,9 @@
 
 # Vérifier si l'URL a été passée en argument
 if [ -z "$1" ]; then
-  echo "Usage: $0 <URL de la vidéo YouTube>"
+  echo "Usage: $0 *"
   exit 1
 fi
-
-# Créer le répertoire 'thumbnails' s'il n'existe pas
-echo "Création du répertoire thumbnails..."
-mkdir -p thumbnails
-
-# Vérifier si le répertoire a bien été créé
-if [ -d "thumbnails" ]; then
-  echo "Le répertoire thumbnails a été créé avec succès."
-else
-  echo "Erreur : Le répertoire thumbnails n'a pas pu être créé."
-  exit 1
-fi
-
-# Vérifier que la miniature a bien été téléchargée dans le répertoire 'thumbnails'
-echo "Fichiers dans thumbnails :"
-ls -l thumbnails
 
 # Extraire l'ID de la vidéo à partir de l'URL
 URL=$1
@@ -33,8 +17,11 @@ if [ -z "$VIDEO_ID" ]; then
 fi
 
 # Télécharger la miniature en haute définition
-curl -s -o "thumbnails/${VIDEO_ID}_thumbnail.jpg" "https://img.youtube.com/vi/$VIDEO_ID/maxresdefault.jpg"
+THUMBNAIL_URL="https://img.youtube.com/vi/$VIDEO_ID/maxresdefault.jpg"
 
+# Sauvegarder l'image avec un nom clair
+OUTPUT_FILE="thumbnail_${VIDEO_ID}.jpg"
+curl -o "$OUTPUT_FILE" "$THUMBNAIL_URL"
 
 # Vérifier si le téléchargement a réussi
 if [ $? -eq 0 ]; then
